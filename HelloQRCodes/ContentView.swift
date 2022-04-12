388e7dbffc86c70285e2c69d3de528bb2b5ad6f8
39shin52 <b1018088@fun.ac.jp>
2022-04-12 14:44:44 +0900

    Initial Commit

---
+++ HelloQRCodes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloQRCodes
+//
+//  Created by Itou Shingo on 2022/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloQRCodes/HelloQRCodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloQRCodesApp.swift
+//  HelloQRCodes
+//
+//  Created by Itou Shingo on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct HelloQRCodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
